a8722317e02efe9802219069d492d6764e357594
Colby Garland <colby@teleoinc.com>
2020-04-14 20:05:01 -0600

    Scroll only if page height big enough

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,10 @@
-const body = document.querySelector('body');
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+console.log(height);
 
 // OnScroll event handler
 const onScroll = () => {
@@ -7,7 +13,7 @@ const onScroll = () => {
     const scroll = document.documentElement.scrollTop;
 
     // If scroll value is more than 0 - add class
-    if (scroll > 0) {
+    if (scroll > 0 && height > 900) {
         body.classList.add("scrolled");
     } else {
         body.classList.remove("scrolled")
